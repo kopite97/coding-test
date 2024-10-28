c702337c9414319f367dad79c59172456724ed68
joon <tjrwns3428@gmail.com>
2024-10-28 19:00:33 +0900

    ë°1일ì백준 1072번 문제

---
+++ src/main/java/com/sparta/club99/Baekjoon1072/Backjoon1072.java
@@ -0,0 +1,33 @@
+package com.sparta.club99.Baekjoon1072;
+
+import java.util.Scanner;
+
+public class Backjoon1072 {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        long all = scanner.nextInt();
+        long wins = scanner.nextInt();
+
+        long rate = (wins*100)/all;
+
+
+        if(rate >= 99){
+            System.out.println(-1);
+            return;
+        }
+
+        long count =0;
+
+        while((wins+count)*100<(rate+1)*(all+count)){
+            count++;
+        }
+
+        System.out.println(count);
+
+        scanner.close();
+    }
+
+}
