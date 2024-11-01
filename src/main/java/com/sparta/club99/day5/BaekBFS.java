1fe7e30091897ac1fa622d078fa47e75530d62df
joon <tjrwns3428@gmail.com>
2024-11-02 00:21:20 +0900

    5일차 : 너비 우선 탐색 BFS

---
+++ src/main/java/com/sparta/club99/day5/BaekBFS.java
@@ -0,0 +1,28 @@
+package com.sparta.club99.day5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BaekBFS {
+
+    static ArrayList<Integer>[] graph;
+    static int[] order;
+    static boolean[] visited;
+    static int counter = 1;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+
+
+    }
+}
