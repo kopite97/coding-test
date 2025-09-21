4e74e4e7c56854b9cc0430cad308ecc69e54cfda
joon <tjrwns3428@gmail.com>
2025-09-22 08:19:08 +0900

    백준 : 10828 스ㅐㅌㄱ

--- src/stack.java
+++ src/stack.java
@@ -1,6 +1,61 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 public class stack {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        int numberCount = 0;
+        String push = "push";
+        String pop = "pop";
+        String size = "size";
+        String empty = "empty";
+        String top = "top";
+
+        Scanner sc = new Scanner(System.in);
+        numberCount = sc.nextInt();
+        sc.nextLine();
+
+        Stack<Integer> stack = new Stack<Integer>();
+
+        while (numberCount > 0) {
+
+            String order = sc.nextLine();
+
+
+            if (order.equals(top)) {
+                if (stack.isEmpty()) {
+                    System.out.println(-1);
+                    numberCount--;
+                    continue;
+                }
+                int topNumber = stack.peek();
+                System.out.println(topNumber);
+
+            } else if (order.equals(size)) {
+                int sizeNumber = stack.size();
+                System.out.println(sizeNumber);
+
+            } else if (order.equals(empty)) {
+                int isEmptyNumber = stack.isEmpty() ? 1 : 0;
+                System.out.println(isEmptyNumber);
+
+            } else if (order.equals(pop)) {
+                if (stack.isEmpty()) {
+                    System.out.println(-1);
+                    numberCount--;
+                    continue;
+                }
+                int popNumber = stack.pop();
+                System.out.println(popNumber);
+
+            } else if (order.substring(0,4).equals(push)) {
+                int number = Integer.parseInt(order.substring(5));
+                stack.push(number);
+            }
+
+            numberCount--;
+        }
+
+        sc.close();
     }
 }
