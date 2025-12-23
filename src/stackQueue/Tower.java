b32b25f4c929fb0b3ad35a3c9f1bfb5b118d5e63
Joon <tjrwns3428@gmail.com>
2025-12-24 01:51:11 +0900

    모ë모노톤 스택 문제 : 탑

--- src/stackQueue/Tower.java
+++ src/stackQueue/Tower.java
@@ -1,2 +1,50 @@
-package stackQueue;public class Tower {
+package stackQueue;
+
+/**
+ * https://www.acmicpc.net/problem/2493
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Tower {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] heights = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+
+            heights[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+
+            while (!stack.isEmpty() && heights[stack.peek()] < heights[i]) {
+                int index = stack.pop();
+            }
+
+            if(stack.isEmpty()){
+                result[i] = 0;
+            } else{
+                result[i] = stack.peek() +1;
+            }
+            stack.push(i);
+        }
+
+
+        StringBuilder sb = new StringBuilder();
+        for (int index : result) {
+            sb.append(index).append(" ");
+        }
+
+        System.out.println(sb);
+
+    }
 }
